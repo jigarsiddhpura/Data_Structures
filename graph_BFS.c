c643eed1439dc25193fb33609e9976985c25cd11
Jigar <95428432+jigarsiddhpura@users.noreply.github.com>
2022-12-10 14:41:52 +0530

    commit 2

--- graph_BFS.c
+++ graph_BFS.c
@@ -1,99 +1,96 @@
 #include <stdio.h>
-#include <conio.h>
-#define max 30
-int adj_mat[20][20], queue[max], n, rear = -1, front = -1;
-int visited[max];
+#include <stdlib.h>
 
-void deque();
-void enque(int val);
-
-void BFS(int v)
+struct queue
 {
-    while (front != -1)
-    {
-        deque(v);
-        printf("%d ", v);
-        visited[v] = 1;
+    int size;
+    int front;
+    int rear;
+    int *arr;
+};
 
-        for (int i = 1; i <= n; i++)
-            if (adj_mat[v][i])
-            {
-                enque(i);
-                // printf("enque of %d \n", i);
-            }
-        for (int i = front; i <= rear; i++)
+int isEmpty(struct queue *q)
+{
+    if (q->rear == q->front)
+    {
+        return 1;
+    }
+    return 0;
+}
+
+int isFull(struct queue *q)
+{
+    if (q->rear == q->size - 1)
+    {
+        return 1;
+    }
+    return 0;
+}
+
+void enqueue(struct queue *q, int val)
+{
+    if (isFull(q))
+    {
+        printf("This Queue is full\n");
+    }
+    else
+    {
+        q->rear++;
+        q->arr[q->rear] = val;
+        printf("Enqued element: %d\n", val);
+    }
+}
+
+int dequeue(struct queue *q)
+{
+    int a = -1;
+    if (isEmpty(q))
+    {
+        printf("This Queue is empty\n");
+    }
+    else
+    {
+        q->front++;
+        a = q->arr[q->front];
+    }
+    return a;
+}
+
+int main()
+{
+    // Initializing Queue (Array Implementation)
+    struct queue q;
+    q.size = 400;
+    q.front = q.rear = 0;
+    q.arr = (int *)malloc(q.size * sizeof(int));
+
+    // BFS Implementation
+    int node;
+    int i = 1;
+    int visited[7] = {0, 0, 0, 0, 0, 0, 0};
+    int a[7][7] = {
+        {0, 1, 1, 1, 0, 0, 0},
+        {1, 0, 1, 0, 0, 0, 0},
+        {1, 1, 0, 1, 1, 0, 0},
+        {1, 0, 1, 0, 1, 0, 0},
+        {0, 0, 1, 1, 0, 1, 1},
+        {0, 0, 0, 0, 1, 0, 0},
+        {0, 0, 0, 0, 1, 0, 0}};
+    printf("%d", i);
+    visited[i] = 1;
+    enqueue(&q, i); // Enqueue i for exploration
+    while (!isEmpty(&q))
+    {
+        int node = dequeue(&q);
+        for (int j = 0; j < 7; j++)
         {
-            BFS(queue[i]);
+            if (a[node][j] == 1 && visited[j] == 0)
+            {
+                printf("%d", j);
+                visited[j] = 1;
+                enqueue(&q, j);
+            }
         }
     }
-}
-
-void enque(int val)
-{
-    if (rear == max - 1)
-    {
-        printf("queue overflow");
-    }
-    else if (front == -1)
-    {
-        front++;
-    }
-    rear++;
-    queue[rear] = val;
-}
-
-void deque()
-{
-    if (front == -1)
-    {
-        printf("queue underflow");
-    }
-    else if (front == rear)
-    {
-        front = rear = -1;
-    }
-    else
-    {
-        front++;
-    }
-}
-
-void main()
-{
-    int i, j, count = 0;
-    printf("\n Enter number of vertices:");
-    scanf("%d", &n);
-    for (i = 1; i <= n; i++)
-    {
-        visited[i] = 0;
-        for (j = 1; j <= n; j++)
-            adj_mat[i][j] = 0;
-    }
-
-    // printf("\n Enter the adjacency matrix:\n");
-    // for (i = 1; i <= n; i++)
-    //     for (j = 1; j <= n; j++)
-    //         scanf("%d", &adj_mat[i][j]);
-
-    int adj_mat[5][5] = {
-        {0,1,0,0,1},
-        {1,0,1,1,0},
-        {0,1,0,0,0},
-        {0,1,0,0,0},
-        {1,0,0,0,0}
-    };
-
-    enque(1);
-    BFS(1);
-    printf("\n");
-
-    for (i = 1; i <= n; i++)
-    {
-        if (visited[i])
-            count++;
-    }
-    if (count == n)
-        printf("\n Graph is connected");
-    else
-        printf("\n Graph is not connected");
+    return 0;
 }
