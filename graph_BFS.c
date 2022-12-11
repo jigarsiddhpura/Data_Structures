0e138aa1be436c7122d8c86eca6c337076aa2c26
Jigar <95428432+jigarsiddhpura@users.noreply.github.com>
2022-12-11 16:48:36 +0530

    commit BFS

--- graph_BFS.c
+++ graph_BFS.c
@@ -1,96 +1,113 @@
+
 #include <stdio.h>
-#include <stdlib.h>
+#define Max 5
+int f = -1;
+int r = -1;
+int a[Max];
 
-struct queue
-{
-    int size;
-    int front;
-    int rear;
-    int *arr;
-};
+// a = adjacency matrix , v = visited array
 
-int isEmpty(struct queue *q)
+void insert(int val)
 {
-    if (q->rear == q->front)
+    if (r == Max - 1)
+        printf("Overflow");
+    else if (f == -1 && r == -1)
     {
-        return 1;
-    }
-    return 0;
-}
-
-int isFull(struct queue *q)
-{
-    if (q->rear == q->size - 1)
-    {
-        return 1;
-    }
-    return 0;
-}
-
-void enqueue(struct queue *q, int val)
-{
-    if (isFull(q))
-    {
-        printf("This Queue is full\n");
+        f = r = 0;
     }
     else
-    {
-        q->rear++;
-        q->arr[q->rear] = val;
-        printf("Enqued element: %d\n", val);
-    }
+        r++;
+
+    a[r] = val;
 }
 
-int dequeue(struct queue *q)
+int delete()
 {
-    int a = -1;
-    if (isEmpty(q))
-    {
-        printf("This Queue is empty\n");
-    }
+    if (f == -1 && r == -1)
+        return -10;
     else
     {
-        q->front++;
-        a = q->arr[q->front];
-    }
-    return a;
-}
-
-int main()
-{
-    // Initializing Queue (Array Implementation)
-    struct queue q;
-    q.size = 400;
-    q.front = q.rear = 0;
-    q.arr = (int *)malloc(q.size * sizeof(int));
-
-    // BFS Implementation
-    int node;
-    int i = 1;
-    int visited[7] = {0, 0, 0, 0, 0, 0, 0};
-    int a[7][7] = {
-        {0, 1, 1, 1, 0, 0, 0},
-        {1, 0, 1, 0, 0, 0, 0},
-        {1, 1, 0, 1, 1, 0, 0},
-        {1, 0, 1, 0, 1, 0, 0},
-        {0, 0, 1, 1, 0, 1, 1},
-        {0, 0, 0, 0, 1, 0, 0},
-        {0, 0, 0, 0, 1, 0, 0}};
-    printf("%d", i);
-    visited[i] = 1;
-    enqueue(&q, i); // Enqueue i for exploration
-    while (!isEmpty(&q))
-    {
-        int node = dequeue(&q);
-        for (int j = 0; j < 7; j++)
+        int t = a[f];
+        f++;
+        if (f > r)
         {
-            if (a[node][j] == 1 && visited[j] == 0)
+            f = r = -1;
+        }
+        return t;
+    }
+}
+
+int isempty()
+{
+    if (f == -1 && r == -1)
+        return -10;
+    else
+    {
+        return a[f];
+    }
+}
+
+void display()
+{
+    if (f == -1 && r == -1)
+        printf("Under flow");
+    else
+    {
+        for (int i = f; i <= r; i++)
+        {
+            printf("%d\t", a[i]);
+        }
+    }
+}
+
+void main()
+{
+    int n = 5, v[100];
+    // i is the start vertex
+    int i ;
+    int a[100][100];
+    printf("Enter the size of matrix : ");
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        printf("\nEnter the %d row : ", i + 1);
+        for (int j = 0; j < n; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        v[i] = 0;
+    }
+
+    printf("Adjacency matrix : ");
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("\n");
+        for (int j = 0; j < n; j++)
+        {
+            printf(" %d ", a[i][j]);
+        }
+    }
+
+    printf("\nEnter the starting vertex : ");
+    scanf("%d",&i);
+    printf("%d", i);
+    v[i] = 1;
+    insert(i);
+    while (isempty() != -10)
+    {
+        int node = delete ();
+        for (int j = 0; j < n; j++)
+        {
+            if (a[node][j] == 1 && v[j] == 0)
             {
                 printf("%d", j);
-                visited[j] = 1;
-                enqueue(&q, j);
+                v[j] = 1;
+                insert(j);
             }
         }
     }
-    return 0;
 }
